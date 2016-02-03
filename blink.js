10d0e88b03729afa461b173cf61d0634f06c2b6d
SoNiros <ssosoghe@yahoo.com>
2016-02-03 23:28:14 +0200

    Updated exercise

--- blink.js
+++ blink.js
@@ -10,11 +10,4 @@
 
             setInterval(function() {
                 blinktext('blink-1','#d2c19a');
-                blinktext('blink-2','#d2c19a');
-				/*blinktext('blink-2','#d2c19a');
-				blinktext('blink-3','#d2c19a');
-				blinktext('blink-4','#d2c19a');
-				blinktext('blink-5','#d2c19a');
-				blinktext('blink-6','#d2c19a');
-				blinktext('blink-7','#d2c19a');*/
             }, 500 ); //set an interval timer up to repeat the function
